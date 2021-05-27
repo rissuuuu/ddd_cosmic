ec1621751b711f3924b480e285817b1e5d266520
Rishav Pudel <rissuuuu@gmail.com>
2021-05-27 20:24:04 +0545

    Refactored UOW() instance from handlers

--- entrypoint/app.py
+++ entrypoint/app.py
@@ -1,14 +1,13 @@
-import  json
 import redis
 from sanic import Sanic
 from sanic import response
 
-from domain import events, commands
-from service_layer import unit_of_work, messagebus,handlers
+from domain import commands
+from service_layer import unit_of_work, messagebus, handlers
 
 app = Sanic(__name__)
 # app.email = emailclass
-# app.db = database_instance
+# app.db = database_instances
 # app.redis = redis_connection
 
 r = redis.Redis(host='127.0.0.1', port=6379)
@@ -29,25 +28,27 @@ async def add_batch(request):
     # return response.text("ok")
 
     command = commands.CreateBatch(ref=ref, sku=sku_, qty=pq_)
-    results = await messagebus.handle(message=command,uow=unit_of_work.FakeUnitOfWork)
+    results = await messagebus.handle(message=command, uow=unit_of_work.FakeUnitOfWork())
     return response.text(results.pop(0))
+
 
 @app.route("/get_data", methods=["GET"])
 async def get_all_batches(request):
     batches = await handlers.get_batches(uow=unit_of_work.FakeUnitOfWork)
     return response.text("ok")
 
-@app.route("/get_single_data", methods=["GET"])
-async def get_one_batches(request):
-    batches = handlers.get_batches()
-    return response.text("ok")
 
-@app.route("/allocate",methods=["POST"])
+# @app.route("/get_single_data", methods=["GET"])
+# async def get_one_batches(request):
+#     batches = handlers.get_batches()
+#     return response.text("ok")
+
+@app.route("/allocate", methods=["POST"])
 async def allocate(request):
     sku = request.form.get("sku")
     qty = request.form.get("qty")
     command = commands.Allocate(sku=sku, qty=qty)
-    results = await messagebus.handle(message=command, uow=unit_of_work.FakeUnitOfWork)
+    results = await messagebus.handle(message=command, uow=unit_of_work.FakeUnitOfWork())
     # batchref = results.pop(0)
     return response.text("ok")
 
@@ -56,7 +57,7 @@ async def change_batch_qty(request):
     ref = request.form.get("ref")
     qty = request.form.get("qty")
     command = commands.ChangeBatchQuantity(ref=ref, qty=qty)
-    results=await messagebus.handle(command,unit_of_work.FakeUnitOfWork)
+    results = await messagebus.handle(command, unit_of_work.FakeUnitOfWork())
     return  response.text(results.pop(0))
 
 
