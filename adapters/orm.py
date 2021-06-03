55771117bb35100b0b5906d69b8eba5b9c71ea7a
Rishav Pudel <rissuuuu@gmail.com>
2021-06-03 21:22:00 +0545

    Created ORM

--- adapters/orm.py
+++ adapters/orm.py
@@ -1,15 +1,15 @@
-from sqlalchemy import (
-    Table,
-    Column,
-    Integer,
-    String,
-    MetaData
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
 
-)
+metadata = sa.MetaData()
 
-batches = Table(
+batches = sa.Table(
     "batches",
-    MetaData,
-    Column()
-
+    metadata,
+    sa.Column("ref", postgresql.UUID(as_uuid=False), primary_key=True,unique=True),
+    sa.Column("sku", sa.String(255)),
+    sa.Column("eta", sa.TIMESTAMP(timezone=True),
+              default=sa.func.now()
+              ),
+    sa.Column("purchased_qty",sa.Integer,nullable=False)
 )
